3f92e86cfe527c6db7ce662fff41b8197e02988f
jacksonbrunoss <oi.jbss@gmail.com>
2020-02-01 14:51:33 -0300

    Projeto Techno add carrinho de compras

--- techno-projeto/script/app.js
+++ techno-projeto/script/app.js
@@ -2,21 +2,37 @@ const vue = new Vue({
     el: "#app",
     data: {
         produtos: [],
-        produto: []
+        produto: [],
+        carrinho: []
     },
     filters: {
         numeroPreco(valor) {
-            console.log(valor);
             return valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL" });
+        }
+    },
+    computed: {
+        carrinhoTotal() {
+            let total = 0;
+            if(this.carrinho.length) {
+                this.carrinho.forEach(item => {
+                    total += item.preco;
+                })
+            }
+            return total;
         }
     },
     methods: {
         fetchProdutos() {
-            fetch('./api/produtos.json')
-            .then(r => r.json())
-            .then(r => {
-                this.produtos = r;
-            })
+            try {
+                fetch('./api/produtos.json', {method: 'get'})
+                .then(r => r.json())
+                .then(r => {
+                    this.produtos = r;
+                })
+            }
+            catch(err) {
+                console.log('Aconteceu um erro: ', err);
+            }
         },
         fetchProduto(id) {
             fetch(`./api/produtos/${id}/dados.json`)
@@ -24,9 +40,39 @@ const vue = new Vue({
             .then(r => {
                 this.produto = r;
             })
+        },
+        abrirModal(id) {
+            this.fetchProduto(id);
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            })
+        },
+        fecharModal({target, currentTarget}) {
+            if (target === currentTarget) this.produto = false
+        },
+        ADDItem() {
+            this.produto.estoque--;
+            const {id, nome, preco } = this.produto
+            this.carrinho.push({id, nome, preco});
+        },
+        removeItem(index) {
+            this.carrinho.splice(index, 1)
+            console.log(index);
+        },
+        checarLocal() {
+            if(window.localStorage.carrinho) {
+                this. carrinho = JSON.parse(window.localStorage.carrinho);
+            }
+        }
+    },
+    watch: {
+        carrinho() {
+            window.localStorage.carrinho = JSON.stringify(this.carrinho);
         }
     },
     created() {
         this.fetchProdutos();
+        this.checarLocal();
     },
 })
